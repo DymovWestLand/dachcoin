780552ed5b6cf2194b1b763b3fbaef017361a815
dachcoin <media@dachcoin.live>
2018-12-27 08:33:34 +0300

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -93,7 +93,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0xdc;
+        pchMessageStart[0] = 0xdd;
         pchMessageStart[1] = 0x6c;
         pchMessageStart[2] = 0xf0;
         pchMessageStart[3] = 0x04;
